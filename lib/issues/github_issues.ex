c4daeb811fee5a3ea4482c5175ba9ab9ea3a7b54
Vincent Theron <vptheron@gmail.com>
2015-09-20 17:29:59 -0400

    add logging config and calls

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,11 +1,14 @@
 defmodule Issues.GithubIssues do
 
+  require Logger
+
   @user_agent [ { "User-agent", "Programming Elixir" }]
 
   # Use a module attribute to fetch the value at compile time
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}."
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -17,16 +20,19 @@ defmodule Issues.GithubIssues do
 
   def handle_response(
     {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    Logger.info "Successful response."
     {:ok, :jsx.decode(body)}
   end
 
   def handle_response(
-    {:ok, %HTTPoison.Response{status_code: _, body: body}}) do
+    {:ok, %HTTPoison.Response{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
     {:error, :jsx.decode(body)}
   end
 
   def handle_response(
     {:error, %HTTPoison.Error{reason: reason}}) do
+    Logger.error "Error #{reason}"
     {:error, reason}
   end
 
